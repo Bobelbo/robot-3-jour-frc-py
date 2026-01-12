8e196b8c72aaf7dfd27261f0464020ca2038a91c
P-B3117 <60858078+P-B3117@users.noreply.github.com>
2026-01-11 16:47:53 -0800

    motor and pid implementation

--- subsystems/canMotorSS.py
+++ subsystems/canMotorSS.py
@@ -1,21 +1,83 @@
+from enum import Enum
+
 import rev
+from wpimath.controller import PIDController
 
-from typing import List
-
+from .pid import Pid
 from .subsystemInterface import SubsystemInterface
+
+SPARK_TYPE = rev.SparkMax | rev.SparkFlex
+
+
+class CANMotorType(Enum):
+    SPARKMAX = rev.SparkMax
+    SPARKFLEX = rev.SparkFlex
+
 
 class CANMotorSS(SubsystemInterface):
     """SubSystem for controlling a brushless sparkMax motor controller over CAN bus."""
-    _motor = None
 
-    def __init__(self, can_id: int):
+    _motor: SPARK_TYPE
+
+    def __init__(
+        self,
+        can_id: int,
+        type: CANMotorType,
+        brushType: rev.SparkLowLevel.MotorType = rev.SparkLowLevel.MotorType.kBrushless,
+        pid: Pid | None = None,
+    ):
         super().__init__()
-        self._motor = rev.SparkMax(can_id, rev.SparkLowLevel.MotorType.kBrushless)
+        self._motor = type.value(can_id, brushType)
+        self._motorConfig = rev.SparkMaxConfig()
+        self._stop: bool = False
+        self.encoder = self._motor.getEncoder()
+        self.pid = pid
+
+    def udpate(self) -> None:
+        """
+        Updates the motor output if it is not stopped and a pid is set
+        """
+        if not self._stop and self.pid is not None:
+            self._motor.set(self.pid.getOutput())
+
+    def setBrakeMode(self, mode: bool) -> None:
+        """
+        Sets the idle mode
+        mode: False = coast, True = Brake
+        """
+        self._motorConfig.setIdleMode(
+            rev.SparkBaseConfig.IdleMode.kBrake
+        ) if mode else self._motorConfig.setIdleMode(
+            rev.SparkBaseConfig.IdleMode.kCoast
+        )
+
+        self._configure()
+
+    def _configure(self) -> None:
+        """Updates the motor config"""
+        self._motor.configure(
+            self._motorConfig,
+            rev.ResetMode.kResetSafeParameters,
+            rev.PersistMode.kPersistParameters,
+        )
 
     def set_speed(self, speed: float) -> None:
-        """Set the speed of the motor."""
-        self._motor.set(speed)
+        """Sets the speed of the motor. IF the pid Has been set"""
+        if self.pid is not None:
+            self._stop = False
+            self.pid.setTarget(speed)
+            self._motor.set(self.pid.getOutput())
+
+    def set_output(self, output: float) -> None:
+        """Set the raw output of the motor."""
+        self._motor.set(output)
+        if output == 0:
+            self._stop = True
+
+    def set_pid(self, pid: Pid) -> None:
+        self.pid = pid
 
     def stop(self) -> None:
         """Stop motor."""
+        self._stop = True
         self._motor.set(0.0)
---
+++ subsystems/pid.py
@@ -0,0 +1,47 @@
+from typing import Callable
+
+from wpimath.controller import PIDController
+
+from .subsystemInterface import SubsystemInterface
+
+
+class Pid(SubsystemInterface):
+    def __init__(
+        self,
+        dataGetter: Callable[[], float],
+        kp: float,
+        ki: float,
+        kd=0.0,
+        tolerance=0,
+        noReverse: bool = False,
+    ):
+        self.pidTarget: float = 0
+        self.kP: float = kp
+        self.kI: float = ki
+        self.kD: float = kd
+        self.pidNoReverse = noReverse
+        self.pid = PIDController(self.kP, self.kI, self.kD)
+        self.pid.setTolerance(tolerance)
+        self.currentData = dataGetter
+        self.output: float = 0
+
+    def update(self) -> None:
+        """updates running even if input did not change"""
+
+        currentData = self.currentData()
+        pidOutput = self.pid.calculate(currentData, self.pidTarget)
+
+        # bang bang principle so the shooter just coasts down in speed instead of trying to brake himself
+        if self.pidNoReverse:
+            output = pidOutput if pidOutput > 0 else 0
+        # Normal pid behavior
+        else:
+            output = pidOutput
+
+        self.output = output
+
+    def setTarget(self, target: float) -> None:
+        self.pidTarget = target
+
+    def getOutput(self) -> float:
+        return self.output
