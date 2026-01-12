e4f04e90fba274c0b23d57555f6328e746b698c2
P-B3117 <60858078+P-B3117@users.noreply.github.com>
2026-01-11 18:47:18 -0800

    voila config

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import wpilib
 
-from commands import FeederAngleCommand, FeederButtonCommand, TurretAngleCommand, TankJoystickCommand, TurretShooterCommand
+from commands import (
+    FeederAngleCommand,
+    FeederButtonCommand,
+    TankJoystickCommand,
+    TurretAngleCommand,
+    TurretShooterCommand,
+)
 from subsystems.canMotorSS import CANMotorSS, CANMotorType
 
 # We define base wpilib instances so we do not create / destroy in each lambda call
@@ -17,17 +23,17 @@ config = {
         },
         "buttons": {
             # ID : Function inside a tuple
-            ("btn1", lambda: _input_remote.getRawButton(0)),
-            ("btn2", lambda: _input_remote.getRawButton(1)),
-            ("btn3", lambda: _input_remote.getRawButton(2)),
-            ("btn4", lambda: _input_remote.getRawButton(3)),
-            ("btn5", lambda: _input_remote.getRawButton(4)),
-            ("btn6", lambda: _input_remote.getRawButton(5)),
-            ("btn7", lambda: _input_remote.getRawButton(6)),
-            ("btn8", lambda: _input_remote.getRawButton(7)),
-            ("btn9", lambda: _input_remote.getRawButton(8)),
-            ("btn10", lambda: _input_remote.getRawButton(9)),
-            ("btn11", lambda: _input_remote.getRawButton(10)),
+            ("btn1", lambda: _input_remote.getRawButton(1)),
+            ("btn2", lambda: _input_remote.getRawButton(2)),
+            ("btn3", lambda: _input_remote.getRawButton(3)),
+            ("btn4", lambda: _input_remote.getRawButton(4)),
+            ("btn5", lambda: _input_remote.getRawButton(5)),
+            ("btn6", lambda: _input_remote.getRawButton(6)),
+            ("btn7", lambda: _input_remote.getRawButton(7)),
+            ("btn8", lambda: _input_remote.getRawButton(8)),
+            ("btn9", lambda: _input_remote.getRawButton(9)),
+            ("btn10", lambda: _input_remote.getRawButton(10)),
+            ("btn11", lambda: _input_remote.getRawButton(11)),
         },
     },
     # Config for actuators and sensors
@@ -37,15 +43,27 @@ config = {
     "commands": [
         # Initialize your commands here\
         FeederButtonCommand("btn2", CANMotorSS(5, CANMotorType.SPARKFLEX)),
-        FeederAngleCommand(["btn11", "btn10"], CANMotorSS(6, CANMotorType.SPARKMAX)),
-        TurretShooterCommand(
-            button="btn0",
-            shootMotor=CANMotorSS(10, CANMotorType.SPARKFLEX),
-            feedMotor=CANMotorSS(11, CANMotorType.SPARKMAX),
-        ),
-        TankJoystickCommand(["baseForwardAxis","baseRotationAxis","btn9"],
-                            [CANMotorSS(1, CANMotorType.SPARKMAX),CANMotorSS(2, CANMotorType.SPARKMAX)],
-                            [CANMotorSS(3, CANMotorType.SPARKMAX),CANMotorSS(4, CANMotorType.SPARKMAX)]),
-        TurretAngleCommand(["baseForwardAxis","baseRotationAxis","btn9"],CANMotorSS(8, CANMotorType.SPARKMAX),CANMotorSS(9, CANMotorType.SPARKMAX)),
+        # FeederAngleCommand(["btn11", "btn10"], CANMotorSS(6, CANMotorType.SPARKMAX)),
+        # TurretShooterCommand(
+        #     button="btn1",
+        #     shootMotor=CANMotorSS(10, CANMotorType.SPARKFLEX),
+        #     feedMotor=CANMotorSS(11, CANMotorType.SPARKMAX),
+        # ),
+        # TurretAngleCommand(
+        #     ["baseForwardAxis", "baseRotationAxis", "btn9"],
+        #     CANMotorSS(8, CANMotorType.SPARKMAX),
+        #     CANMotorSS(9, CANMotorType.SPARKMAX),
+        # ),
+        # TankJoystickCommand(
+        #     ["baseForwardAxis", "baseRotationAxis", "btn9"],
+        #     [
+        #         CANMotorSS(1, CANMotorType.SPARKMAX),
+        #         CANMotorSS(2, CANMotorType.SPARKMAX),
+        #     ],
+        #     [
+        #         CANMotorSS(3, CANMotorType.SPARKMAX),
+        #         CANMotorSS(4, CANMotorType.SPARKMAX),
+        #     ],
+        # ),
     ],
 }
